be70e3e5ad980b85f5fd47bd64004f36f079d2bf
Jess <>
2018-04-11 19:48:00 -0300

    JDP 3

--- drumwave/seven/classes/Texts.js
+++ drumwave/seven/classes/Texts.js
@@ -5,7 +5,7 @@ function Texts(){
 	
 	this.arrow;
 	
-	var title = "<fcWhite><fs2- 7 sets Venn Diagram - <br />128 color combinations from mixing 7 colors</f></f>";
+	var title = "<fcWhite><fs24>- 7 sets Venn Diagram - <br />128 color combinations from mixing 7 colors</f></f>";
 	
 	var text = " ";
 	
